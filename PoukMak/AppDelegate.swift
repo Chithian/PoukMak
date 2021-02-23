f4897e21c62dc298c6f610c53b0171688fb9f73c
Chithian(Hunter) <chithiansamony@gmail.com>
2021-02-23 08:48:44 +0700

    clean code and update pod

--- PoukMak/AppDelegate.swift
+++ PoukMak/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-//import IQKeyboardManagerSwift
 
 
 @main
@@ -16,15 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        
-//        IQKeyboardManager.shared.enable = true
-//
-//        IQKeyboardManager.shared.enableAutoToolbar = false
-//        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
-//
-//
-        
         
         
         return true
